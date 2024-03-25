21f37272db46651175df3dbf0b0567118c023e3b
AbSteg <123431923+AbSteg@users.noreply.github.com>
2024-03-25 17:20:11 -0500

    Add files via upload

---
+++ 08Week/model/database.php
@@ -0,0 +1,19 @@
+<?php
+    $dsn = 'mysql:host=pxukqohrckdfo4ty.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=hnwmc5g21co1445v';
+    $username = 'jrlvsjo47diaivlp';
+    $password = 'hkkaaztkrtgmb375';
+   /*$dsn = 'mysql:host=localhost;dbname=zippyusedautos';
+    $username = 'root';
+    $password = 'pa55word';*/
+
+
+    try {
+        $db = new PDO($dsn, $username);
+    } catch (PDOException $e)
+    {
+        $error = "Database Error: ";
+        $error .= $e -> getMessage();
+        include ('view/error.php');
+        exit();
+    }
+?>
